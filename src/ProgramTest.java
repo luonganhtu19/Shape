0cec4788eb2af8e75335bf4b6edc5eb5630c2f0d
- <->
2020-11-15 20:09:25 +0700

    comparatorVRS2

--- src/ProgramTest.java
+++ src/ProgramTest.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.lang.reflect.Array;
 import java.util.Arrays;
+import java.util.Comparator;
 
 public class ProgramTest {
     public static void main(String[] args) {
@@ -8,7 +9,7 @@ public class ProgramTest {
         System.out.println(shape);
 
         long start=System.currentTimeMillis();
-        shape=new Shape("red",false);
+        shape=new Shape("yellow",false);
         System.out.println(shape);
 
 
@@ -40,10 +41,14 @@ public class ProgramTest {
         square=new Square(5.8,"yellow",true);
         System.out.println(square);
 
-        ComparableCircle[] circles=new ComparableCircle[3];
-        circles[0]=new ComparableCircle(3.6);
+        ComparableCircle[] circles=new ComparableCircle[5];
+        circles[0]=new ComparableCircle(3.5);
         circles[1]=new ComparableCircle();
-        circles[2]=new ComparableCircle(3.5,"indigo",false);
+        circles[2]=new ComparableCircle(7);
+        circles[4]=new ComparableCircle(3.5,"indigo",false);
+
+        circles[3]=new ComparableCircle(7,"while",false);
+
 
         System.out.println("Pre-sorted");
         for (ComparableCircle circle1:circles){
@@ -51,6 +56,26 @@ public class ProgramTest {
         }
         Arrays.sort(circles);
         System.out.println("After-sorted");
+        for (ComparableCircle circle1:circles){
+            System.out.println(circle1);
+        }
+
+        Arrays.sort(circles,new CircleComparator());
+        System.out.println("After-sorted-comparator");
+        for (ComparableCircle circle1:circles){
+            System.out.println(circle1);
+        }
+
+        Arrays.sort(circles, new Comparator<ComparableCircle>() {
+            @Override
+            public int compare(ComparableCircle o1, ComparableCircle o2) {
+                if (o1.getRadius()>o2.getRadius()) return -1;
+                else if(o1.getRadius()<o2.getRadius()) return 1;
+                else
+                    return o1.getColor().compareTo(o2.getColor());
+            }
+        });
+        System.out.println("After-sorted-comparator-2if");
         for (ComparableCircle circle1:circles){
             System.out.println(circle1);
         }
